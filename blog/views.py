47a19ea1901f317a6b167dc3ab9476c7b770a950
Pazzword <anzor.pozner@gmail.com>
2024-03-28 14:33:17 +0000

    Main part

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,8 @@ def post_list(request):
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
