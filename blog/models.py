abcd23527de7e121e405eb54ce341713074eead6
Admin <admin@ANZOR-iMac.local>
2024-04-04 19:25:09 +0100

    Authorisation removed

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,9 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
@@ -37,7 +36,6 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
